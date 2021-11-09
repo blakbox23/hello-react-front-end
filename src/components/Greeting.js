918e23f8752b1e5ff9c6745cf946a087604b6fda
blakbox23 <boudii30@gmail.com>
2021-11-09 20:51:38 +0300

    Add Greeting component with react-router

---
+++ src/components/Greeting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Greeting = () => {
+const message = 'The fake message';
+
+return ( <div>
+    <h1>
+        {message}
+    </h1>
+    </div>)
+}
+
+export default Greeting;
